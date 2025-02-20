3913d1a99562d87150ac41eedf5674da96697721
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-02-20 04:25:13 +0000

    finish practice problem

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,4 +1,4 @@
-
+ 
 class Notes {
 	public static void main(String[] args) {
 
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,33 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static String pluralize(String s) {
+		
+		String w = s.toLowerCase();
+		if(w.endsWith("ey"))
+			return "eys";
+		else if(w.endsWith("y")) 
+			return "ies";
+		else if(w.endsWith("ife"))
+			return "ives";
+		else
+			return "s";
+		}
+	public static int min(int x, int y, int z) {
+		return Math.min(x, Math.min(y, z));
+	}
+	public static boolean isLeapYear(int year) {
+		if(year % 4 == 0) {
+			if(year % 100 == 0) {
+				if(year % 400 == 0) {
+					return true;
+				}
+			}else{
+				return true;
+			}
+		}
+		return false;
+		
+	}
 
 }
